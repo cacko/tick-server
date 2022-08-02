c386da6550ddc112622881b0d7b16b8a2cd504ad
cacko <alex@cacko.net>
2022-08-02 08:11:02 +0100

    I am Root. We are Root.

--- app/lametric/client.py
+++ app/lametric/client.py
@@ -34,21 +34,17 @@ class Client(object):
         url = self.__config.widget_endpoint
         token = self.__config.widget_token
         logging.info(args)
-        try:
-            response = requests.request(
-                method=method.value,
-                headers={
-                    'X-Access-Token': token,
-                    'Cache-Control': 'no-cache',
-                    'Accept': 'application/json'
-                },
-                url=f"{url}",
-                **args
-            )
-            logging.info(response.json())
-            return response.status_code
-        except Exception as e:
-            print_exc(e)
+        response = requests.request(
+            method=method.value,
+            headers={
+                'X-Access-Token': token,
+                'Cache-Control': 'no-cache',
+                'Accept': 'application/json'
+            },
+            url=f"{url}",
+            **args
+        )
+        return response.status_code
 
     def send_notification(self, notification: Notification):
         data = notification.to_dict()
