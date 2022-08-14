6dad5f6e31a15652527cf8cb7635d5b4ed3a4715
cacko <alex@cacko.net>
2022-08-14 21:35:47 +0100

    ajax-loader hotness, oh yeah

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-from os import environ
-
-logging.basicConfig(
-    level=getattr(logging, environ.get("TICK_LOG_LEVEL", "DEBUG")),
-    format="%(filename)s %(message)s",
-    datefmt="%H:%M:%S",
-)
--- app/__main__.py
+++ app/__main__.py
@@ -2,6 +2,14 @@
 
 import logging
 from app.core.app import App
+from os import environ
+
+logging.basicConfig(
+    level=getattr(logging, environ.get("TICK_LOG_LEVEL", "INFO")),
+    format="%(filename)s %(message)s",
+    datefmt="%H:%M:%S",
+)
+
 
 try:
     App.start()
