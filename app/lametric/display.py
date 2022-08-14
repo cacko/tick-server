e1911ddb458b66fc060010dfa6b21422d8caddfb
cacko <alex@cacko.net>
2022-08-14 22:08:38 +0100

    if you're not using et, fuck off

--- app/lametric/display.py
+++ app/lametric/display.py
@@ -107,7 +107,7 @@ class Display(object):
             self._current_idx = next_idx
         else:
             self._current_idx = 0
-            self._device_display = self._client.get_display()
+            # self._device_display = self._client.get_display()
 
     def update(self):
         if self.is_screensaver_active:
