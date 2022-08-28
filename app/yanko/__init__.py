518f0e6743cd5e8de8862e1adc651018979666f7
cacko <alex@cacko.net>
2022-08-29 00:18:53 +0100

    Derp, asset redirection in dev mode

--- app/yanko/__init__.py
+++ app/yanko/__init__.py
@@ -47,7 +47,7 @@ class Yanko(object, metaclass=YankoMeta):
                 headers=self.__otp.headers,
                 **kwags
             )
-            return resp.content.json()
+            return resp.json()
         except Exception as e:
             logging.debug(e)
             return False
