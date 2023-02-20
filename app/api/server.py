425d8621fdc066015f4196db41f16ccdca78a122
cacko <alex@cacko.net>
2023-02-20 03:04:32 +0000

    if you're not using et, fuck off

--- app/api/server.py
+++ app/api/server.py
@@ -46,6 +46,7 @@ class Server(object, metaclass=ServerMeta):
         srv.start()
 
     def handle_nowplaying(self, payload):
+        logging.info(payload)
         LaMetric.queue.put_nowait((CONTENT_TYPE.NOWPLAYING, payload))
 
     def handle_status(self, payload):
