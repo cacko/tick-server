6ec389a3c0df8b0ff85f0eecd8cee0271467342d
cacko <alex@cacko.net>
2024-06-19 16:53:11 +0100

    See last commit

--- app/scheduler/__init__.py
+++ app/scheduler/__init__.py
@@ -1,13 +1,10 @@
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
-
-
-class RedisNotConfiguredException(Exception):
-    pass
+from typing import Optional
 
 
 class SchedulerMeta(type):
-    _instance = None
+    _instance: Optional['Scheduler'] = None
 
     def __call__(cls, *args, **kwargs):
         if not cls._instance:
@@ -15,35 +12,33 @@ class SchedulerMeta(type):
         return cls._instance
 
     def start(cls):
-        cls().scheduler.start()
+        cls()._scheduler.start()
 
     def stop(cls):
-        try:
-            cls().scheduler.shutdown()
-        except Exception:
-            pass
+        cls()._scheduler.shutdown()
 
     def add_job(cls, *args, **kwargs):
-        return cls().scheduler.add_job(*args, **kwargs)
+        return cls()._scheduler.add_job(*args, **kwargs)
 
     def get_job(cls, id, jobstore=None):
-        return cls().scheduler.get_job(id, jobstore)
+        return cls()._scheduler.get_job(id, jobstore)
 
     def cancel_jobs(cls, id, jobstore=None):
-        return cls().scheduler.remove_job(id, jobstore)
+        return cls()._scheduler.remove_job(id, jobstore)
 
     def remove_all_jobs(cls, jobstore=None):
-        return cls().scheduler.remove_all_jobs(jobstore)
+        return cls()._scheduler.remove_all_jobs(jobstore)
 
     def get_jobs(cls, jobstore=None, pending=None):
-        return cls().scheduler.get_jobs(jobstore, pending)
+        return cls()._scheduler.get_jobs(jobstore, pending)
 
 
 class Scheduler(object, metaclass=SchedulerMeta):
 
-    scheduler: BackgroundScheduler
+    _scheduler: BackgroundScheduler = None
+    _instance = None
 
     def __init__(self) -> None:
-        self.scheduler = BackgroundScheduler(
+        self._scheduler = BackgroundScheduler(
             jobstores=dict(default=MemoryJobStore())
         )
